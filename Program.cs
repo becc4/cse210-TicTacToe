233816efe254b82955569f78ea6c8b5b07ecf687
Becca Roeth <roe21002@byui.edu>
2022-09-25 22:16:09 -0600

    Debugging Program

--- Program.cs
+++ Program.cs
@@ -5,81 +5,82 @@ namespace Test
 {
     public class Program
     {
-        public static void Main(string[] arg)
+
+        private void displayScreen(List<String> answers) { //void says no return
+        // creates image (performer)
         {
-        // Making list
-        List<string> answers = new List<string>();
-        answers.Add("1"); answers.Add("2"); answers.Add("3"); 
-        answers.Add("4"); answers.Add("5"); answers.Add("6"); 
-        answers.Add("7"); answers.Add("8"); answers.Add("9");
+            Console.WriteLine($"{answers[0]}|{answers[1]}|{answers[2]}");
+            Console.WriteLine($"{answers[3]}|{answers[4]}|{answers[5]}");
+            Console.WriteLine($"{answers[6]}|{answers[7]}|{answers[8]}");
+        }
+        }  
+        private String getInput(String turn) {
+            Console.Write($"{turn}'s turn to choose a square (1-9): ");
+            String answer = Console.ReadLine();
 
-        string turn;
-        string choice;
-        bool continueGame = false;
-
-            class displayScreen(answers); {
-                // creates image (performer)
-                {
-                    Console.WriteLine($"{answers[0]}|{answers[1]}|{answers[2]}");
-                    Console.WriteLine($"{answers[3]}|{answers[4]}|{answers[5]}");
-                    Console.WriteLine($"{answers[6]}|{answers[7]}|{answers[8]}");
-                }
+            return answer;
+        }
+        private List<String> calculateBoard(List<String> answers, String choice, String turn) {
+            // Calculates the answers list, which X and O goes where
+            // inputs: choice (location), turn (X or O)
+            int location = Convert.ToInt32(choice) - 1;
+            if (answers[location] != "X" && answers[location] != "O") {
+                answers[location] = turn;
             }
-            // Get's user input on which X or O
-            class getInput(turn);
+            return answers;
+        }
+
+        private Boolean gameOver(List<String> answers) {
+            // Makes sure game is still going
+            // inputs: answers, player
+            if (answers[0] == answers[1] && answers[1] == answers[2] 
+            || answers[3] == answers[4] && answers[4] == answers[5]
+            || answers[6] == answers[7] && answers[7] == answers[8]
+
+            || answers[0] == answers[3] && answers[3] == answers[6]
+            || answers[1] == answers[4] && answers[4] == answers[7]
+            || answers[2] == answers[5] && answers[5] == answers[8]
+
+            || answers[0] == answers[4] && answers[4] == answers[8]
+            || answers[2] == answers[4] && answers[4] == answers[6]) 
             {
-                Console.WriteLine($"{turn}'s turn to choose a square (1-9): ");
-                string answer = Console.ReadLine();
-
-                return choice;
+                return true;
+            } else {
+                return false;
             }
+        }
 
-            public calculateBoard(answers, string choice, string turn, bool continueGame); {
-                // Calculates the answers list, which X and O goes where
-                // inputs: choice (location), turn (X or O)
+        public static void Main(string[] arg) {
+            // Making list
+            List<String> answers = new List<String>();
+            answers.Add("1"); answers.Add("2"); answers.Add("3"); 
+            answers.Add("4"); answers.Add("5"); answers.Add("6"); 
+            answers.Add("7"); answers.Add("8"); answers.Add("9");
 
-                int location = Convert.ToInt32(choice) - 1;
-                while (continueGame) {
+            Boolean gameOver = false;
+            String turn = "X";
+            string choice;
 
-                }
-                if (answers[location] != "X" || answers[location] != "O") {
-                    answers.Insert(location, turn);
-                }
-                return choice;
-            }
-            gameOver(answers, turn); {
-                // Makes sure game is still going
-                // inputs: answers, player
-                if (answers[0] == answers[1] && answers[1] == answers[2] 
-                || answers[3] == answers[4] && answers[4] == answers[5]
-                || answers[6] == answers[7] && answers[7] == answers[8]
+            // Director - Calls everything
+            Program prog = new Program();
+            while (!gameOver) {
+                prog.displayScreen(answers);
+                choice = prog.getInput(turn);
+                prog.calculateBoard(answers, choice, turn);
+                //prog.displayScreen(answers);
+                gameOver = prog.gameOver(answers);
 
-                || answers[0] == answers[3] && answers[3] == answers[6]
-                || answers[1] == answers[4] && answers[4] == answers[7]
-                || answers[2] == answers[5] && answers[5] == answers[8]
-                
-                || answers[0] == answers[4] && answers[4] == answers[8]
-                || answers[2] == answers[4] && answers[4] == answers[6]) 
-                {
+                if (gameOver) {
                     Console.WriteLine($"{turn} has won!");
-                    Console.WriteLine($"Good game. Thanks for playing!");
-                    return true;
-                } elif {
-                    return false;
+                }
+
+                if (turn == "X") {
+                    turn = "O";
+                } else {
+                    turn = "X";
                 }
             }
-        // Director - Calls everything
-        do {
-            displayScreen(answers);
-            choice = getInput("O");
-            calculateBoard(answers, choice, "0", continueGame);
-            continueGame = gameOver(answers,turn);
-
-            displayScreen(answers);
-            choice = getInput("X");
-            calculateBoard(answers, choice, "X", continueGame);
-            continueGame = gameOver(answers,turn);
-        } while (continueGame != true);
+            Console.WriteLine($"Good game. Thanks for playing!");
         }
      }
 };
